37f81daa21bc877523ee878f454c88d6189d3465
bradisbell <bisbell@homefinder.com>
2015-05-14 15:32:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,117 @@
+# bucketCloner
+Copy your S3 buckets crazy fast!
+
+This tool will copy all objects from one bucket to another using many sockets
+simultaneously.  It **does not** empty the target bucket first.  You will need
+to do that yourself if necessary.
+
+## Installation / Setup
+1. `npm install -g bucketCloner`
+1. Configure your AWS credentials on your system.  http://aws.amazon.com/sdk-for-node-js/
+
+## Usage
+
+### Quick Example
+```
+bucketcloner --source="mybucket" --target="myotherbucket"
+```
+
+### Options
+
+#### --source
+Specifies the name of the bucket to copy **from**.
+
+#### --target
+Specifies the name of the bucket to copy **to**.
+
+#### --maxSockets
+bucketCloner creates a number of socket connections to S3 simultaneously so
+that it can copy many objects at the same time.  Each connection is also
+re-used for many copy requests.  This parameter sets the number of concurrent
+connections to make.
+
+Several hundred is a good value to choose when you have a nice solid connection
+to S3 (such as on an EC2 instance).  For example:
+
+```
+--maxSockets=400
+```
+
+Default: 100
+
+#### --maxRequests
+A queue of requests is maintained at all times, ready to be flushed to sockets
+as they become available.  It is good to tune this value to some small multiple
+of the maximum number of sockets, such as 4x.
+
+```
+--maxRequests=1600
+```
+
+Default: 500
+
+#### --acl
+The original object's ACL is not copied over from the source bucket.  You must
+choose what ACL to apply to objects in the target bucket. Possible choices:
+
+ - `private`
+ - `public-read`
+ - `public-read-write`
+ - `authenticated-read`
+ - `bucket-owner-read`
+ - `bucket-owner-full-control`
+ 
+```
+--acl="public-read"
+```
+
+Default: public-read
+
+#### --storageClass
+The original object's storage class is copied from the source bucket by
+default, but you can choose to override it here.  Possible choices:
+
+ - `STANDARD`
+ - `REDUCED_RUNDANCY`
+ 
+```
+--storageClass=REDUCED_REDUNDANCY
+```
+
+Default: The storage class from the original object in the source bucket.
+
+#### --sslEnabled
+You can choose to enable TLS/SSL connections.  There is some overhead with
+this option, and it is disabled by default.  Note that if you do not use SSL,
+your AWS secret is not exposed.  It is used to sign requests sent to S3, but
+is not sent in the clear itself.  Paths and parameters **are** sent in the
+clear however, so decide for your use case whether or not this option is
+relevant.
+
+```
+--sslEnabled
+```
+
+Default: false
+
+#### --keyMatch
+Sometimes you only want to copy certain objects.  If specified, only keys
+matching the regex in keyMatch will be copied.  Others will be ignored.
+
+```
+--keyMatch="raw\.jpg$"
+```
+
+#### --marker
+If a bucket clone is stopped in the middle, you can use this option to resume
+where you left off.  The `marker` is the key of the last object copied, and is
+output every second in the status information block.
+
+S3 objects are copied alphabetically.  You can also use `marker` to start the
+copying from an arbitrary key in the bucket.
+
+```
+--marker="/i/012345678/raw.jpg"
+```
+
+Default:  None.  Bucket copying starts from beginning by default.
---
+++ app.js
@@ -0,0 +1,143 @@
+#! /usr/bin/env node
+
+var AWS = require('aws-sdk');
+var http = require('http');
+var yargs = require('yargs');
+
+var s3;
+var objectsToCopy = [];
+var maxRequests;
+var currentRequests = 0;
+
+var nextMarker = null;
+var listInProgress = false;
+
+var progress = {
+  succeeded: 0,
+  failed: 0,
+  bytesTransferred: 0
+}
+
+function startRequest() {
+  currentRequests++;
+  var objectToCopy = objectsToCopy.pop();
+
+  s3.copyObject({
+    Bucket: argv.target,
+    CopySource: encodeURIComponent(argv.source) + '/' + objectToCopy.Key,
+    Key: objectToCopy.Key,
+    ACL: argv.acl,
+    MetadataDirective: 'COPY',
+    StorageClass: argv.storageClass || objectToCopy.StorageClass
+  }, function (err, data) {
+    if (err) {
+      console.log('Error with ' + objectToCopy.Key, err);
+      progress.failed++;
+    } else {
+      progress.succeeded++;
+      progress.bytesTransferred += objectToCopy.Size
+    }
+    
+    currentRequests--;
+    setImmediate(startRequests);
+
+  });
+}
+
+function startRequests() {
+  if (!listInProgress && objectsToCopy.length < 10000 && nextMarker !== false) {
+    listInProgress = true;
+    s3.listObjects({
+      Bucket: argv.source,
+      Marker: nextMarker
+    }, function (err, data) {
+      if (err) {
+        console.log('Fatal error while listing objects', err);
+        process.exit(1);
+      }
+      
+      if (data) {
+        if (argv.keyMatch) {
+          data.Contents.forEach(function (object) {
+            if (object.Key.match(argv.keyMatch)) {
+              objectsToCopy.push(object);
+            }
+          });
+        } else {
+          objectsToCopy = objectsToCopy.concat(data.Contents);
+        }
+
+        if (data.IsTruncated) {
+          nextMarker = data.Contents[data.Contents.length-1].Key;
+        } else {
+          nextMarker = false;
+        }  
+      }
+      listInProgress = false;
+      startRequests();
+    });
+  }
+
+  while (currentRequests <= maxRequests && objectsToCopy.length) {
+    startRequest();
+  }
+}
+
+var argv = yargs
+  .usage('Usage: $0 --source=[source bucket name] --target=[target bucket name]')
+  .demand(['source','target'])
+
+  .describe('source', 'The bucket to copy FROM')
+
+  .describe('target', 'The bucket to copy TO')
+
+  .describe('maxSockets', 'Concurrent connection limit for S3 connection')
+  .default('maxSockets', 100)
+
+  .describe('maxRequests', 'Concurrent copy operations (Set to a number greater than maxSockets for best performance.)')
+  .default('maxRequests', 500)
+
+  .describe('acl', 'The canned ACL to apply to copied objects (private | public-read | public-read-write | authenticated-read | bucket-owner-read | bucket-owner-full-control)')
+  .default('acl', 'private')
+
+  .describe('storageClass', 'Override the original storage class with this (STANDARD | REDUCED_REDUNDANCY)')
+  .default('storageClass', null, 'original object storageClass')
+  
+  .boolean('sslEnabled')
+  .describe('sslEnabled', 'Enable TLS/SSL wrapping for S3 connections')
+  .default('sslEnabled', false)
+
+  .describe('keyMatch', 'RegEx to match the keys of objects that will be copied')
+
+  .describe('marker', 'Object key to start LIST operations after (to resume an interrupted bucket copy)')
+
+  .help('h')
+  .alias('h', 'help')
+  .example('$0 --source=mybucket --target=myotherbucket --maxSockets=400 --maxRequests=1600 --acl="public-read" --storageClass="REDUCED_REDUNDANCY" --keyMatch="raw\.jpg$"', 'Copies from mybucket to myotherbucket, many objects at a time, while forcing public access, reduced redundancy, and filtering on object keys ending in raw.jpg.')
+  .argv;
+
+console.log('Arguments', argv);
+
+maxRequests = argv.maxRequests;
+http.globalAgent.maxSockets = argv.maxSockets;
+nextMarker = argv.marker || null;
+
+AWS.config.sslEnabled = argv.sslEnabled;
+var s3 = new AWS.S3();
+
+startRequests();
+
+setInterval(function () {
+  var uptime = process.uptime();
+  console.log({
+    uptime: uptime,
+    listBufferSize: objectsToCopy.length,
+    succeeded: progress.succeeded,
+    failed: progress.failed,
+    gigabytesTransferred: progress.bytesTransferred/1024/1024/1024,
+    megabitsPerSecond: ((progress.bytesTransferred/1024/1024) * 8) / uptime,
+    objectsPerSecond: progress.succeeded / uptime,
+    marker: nextMarker
+  });
+
+}, 1000);
